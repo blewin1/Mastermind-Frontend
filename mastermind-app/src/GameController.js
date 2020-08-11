0dfeb997121196c1dd3b74a1bde78af482ccbdf2
blewin1 <blewin1@gmail.com>
2020-08-11 19:41:48 -0400

    built out some game functionality

--- mastermind-app/src/GameController.js
+++ mastermind-app/src/GameController.js
@@ -1,13 +1,88 @@
 class GameController {
-    constructor(numPegs, numColors) {
-        this.rows = []
-        for(let i = 0; i < 12; i++) {
-            let pegs = []
-            for(let j = 0; j < numPegs, j++) {
-                pegs.push(null)
-            }
-            this.rows.push(pegs)
+    rows = [];
+    activeRow = 0;
+    numColors = 6;
+    numPegs = 4;
+    code = [];
+    constructor(gameToLoad) {
+        if (gameToLoad) {
+            this.loadGame(gameToLoad);
         }
     }
+    newGame() {
+        this.rows = [];
+        this.activeRow = 0;
+        for (let i = 0; i < 12; i++) {
+            let codePegs = new Array(this.numPegs).fill(null);
+            this.rows.push({ codePegs });
+        }
+        for (let i = 0; i < this.numPegs; i++) {
+            this.code.push(Math.floor(Math.random() * this.numColors));
+        }
+    }
+    loadGame(game) {
+        this.rows = game.rows;
+        this.activeRow = game.activeRow;
+        this.numColors = game.numColors;
+        this.numPegs = game.numPegs;
+        this.code = game.code;
+    }
+    exportGame() {
+        return {
+            rows: this.rows,
+            activeRow: this.activeRow,
+            numColors: this.numColors,
+            numPegs: this.numPegs,
+            code: this.code
+        };
+    }
+    checkActiveRow() {
+        // Return false if row was not filled in.
+        if (this.rows[this.activeRow].codePegs.includes(null)) return false;
 
+        const row = [...this.rows[this.activeRow].codePegs];
+
+        // count matches of color AND location
+        let exactMatches = this.code.reduce(
+            (total, el, i) => (el == row[i] ? total + 1 : total),
+            0
+        );
+
+        // count matches of just color independant of location
+        let totalMatches = 0;
+        this.code.forEach((el) => {
+            let i = row.indexOf(el);
+            if (i != -1) {
+                totalMatches++;
+                row.splice(i, 1);
+            }
+        });
+
+        //Create the keyPegs for the row
+        const keyPegs = [];
+        for (let i = 0; i < this.numPegs; i++) {
+            if (i < exactMatches) {
+                keyPegs.push(2);
+            } else if (i < totalMatches) {
+                keyPegs.push(1);
+            } else {
+                keyPegs.push(0);
+            }
+        }
+
+        //Fill in the keyPegs for the row
+        this.rows[this.activeRow].keyPegs = keyPegs;
+
+        //Increment active row
+        this.activeRow++;
+    }
 }
+
+mastermind = new GameController();
+mastermind.newGame();
+
+mastermind.rows[0].codePegs = [2, 2, 1, 1];
+
+console.log(mastermind.code);
+mastermind.checkActiveRow();
+console.log(mastermind.rows[0]);
